3a7f79c884bc2768330cb45731d62f15a9e8462c
Ahvic <34319767+Ahvic@users.noreply.github.com>
2018-02-26 21:23:15 +0100

    Liste chainée fonctionnelle
    sa marche

--- src/Principale.java
+++ src/Principale.java
@@ -8,20 +8,7 @@ public class Principale {
         LC liste = new LC();
 
         lectureFichier fichier = new lectureFichier("C:\\Users\\Fixe\\Documents\\GitHub\\JeuVie2018\\exemple lif\\ACORN.LIF");
-        //liste = fichier.ConvertionFichier();
-
-
-
-        int[] n1 = {1,1};
-        int[] n2 = {1,2};
-        int[] n3 = {1,3};
-
-        liste.ajout(n1);
-        liste.ajout(n2);
-        liste.ajout(n3);
-        liste.ajout(n1);
-        liste.ajout(n2);
-        liste.ajout(n1);
+        liste = fichier.ConvertionFichier();
 
 
         System.out.println(liste.toString());
